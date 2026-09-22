b2c918396129283a112205b638a42d959598f0a7
programadorweb898-code <programadorweb898@gmail.com>
2026-09-22 00:02:57 -0300

    Configurar entidades y migraciones de TypeORM

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,12 +1,17 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { env } from "../config/env.js";
+import { Tenant } from "./entities/tenant.entity.js";
+import { Service } from "./entities/service.entity.js";
+import { Employee } from "./entities/employee.entity.js";
+import { BusinessHour } from "./entities/business-hour.entity.js";
+import { BlockedTime } from "./entities/blocked-time.entity.js";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: env.databaseUrl,
   synchronize: false,
   logging: false,
-  entities: [],
-  migrations: []
+  entities: [Tenant, Service, Employee, BusinessHour, BlockedTime],
+  migrations: ["dist/database/migrations/*.js"]
 });
